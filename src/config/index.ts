accdc264792afd2835d6c4c5ec986427900a4b20
Raiyan109 <kraiyan109@gmail.com>
2025-03-09 16:04:22 +0600

    cloudinary installed

--- src/config/index.ts
+++ src/config/index.ts
@@ -31,4 +31,7 @@ export default {
     email: process.env.ADMIN_EMAIL,
     password: process.env.ADMIN_PASSWORD,
   },
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
 };
